3849b9ca69e920c3327f8b1bed66906d7c40f9ff
Priyanka Mayya <priyankasmayya@gmail.com>
2025-02-11 16:46:19 +0530

    Day1

---
+++ tutorials/Priyanka S Mayya/react-app/src/components/Alert.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+}
+const Alert = ({ children }: Props) => {
+  return <div className="alert alert-primary">{children}</div>;
+};
+export default Alert;
