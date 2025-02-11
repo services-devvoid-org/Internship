b66e0119c0b9965682708f99a8642b9210cd917e
Priyanka S Mayya <priyankasmayya@gmail.com>
2025-02-11 18:58:00 +0530

    Delete tutorials/Priyanka S Mayya/react-app directory

--- tutorials/Priyanka S Mayya/react-app/src/Message.tsx
+++
@@ -1,9 +0,0 @@
-
-//PascalCasing
-function Message() {
-    const name = 'Priyanka';
-    if (name)
-        return <h1>Hello {name}</h1>
-    return <h1>Hello world</h1>
-}
-export default Message;
