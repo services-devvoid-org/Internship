967f4756913115fea20f09b24af792f2863e7919
Sameeksha <mssameeksha@gmail.com>
2025-02-21 17:31:25 +0530

    Day3 Modified

--- Sameeksha/Day3/Portfolio/src/components/AboutMe.jsx
+++ Sameeksha/Day3/Portfolio/src/components/AboutMe.jsx
@@ -5,7 +5,7 @@ import { experience } from '../constants'
 const AboutMe = () => {
   return (
     <div id="aboutMe"
-    className='bg-slate-500 w-screen h-screen flex justify-between'>
+    className='bg-slate-300 w-screen h-screen flex justify-between'>
       <div className='hidden w-1/2 lg:flex justify-center p-5 ml-48 mt-10'><img className="max-h-1/2 w-auto object-contain rounded-e-3xl" src={Img} alt="#" /></div>
       <div className='w-full flex flex-col p-6 mt-28 mr-8'>
         <div className='text-4xl font-extrabold mb-4'>About Me</div>
@@ -13,18 +13,18 @@ const AboutMe = () => {
         collaborative setting, committed to tackling challenges and creating a profound impact through work.</p><br/></div>
         <ul className='flex'><span className='font-semibold'>Skills: </span>
         {skills.map((skill,index)=>(
-          <li key={index} className='px-2 font-bold text-rose-800 rounded-lg'>{skill}</li>
+          <li key={index} className='px-2 font-bold text-rose-600 rounded-lg'>{skill}</li>
         ))}
           </ul>
           <ul className='flex flex-col mt-6'><span className='font-semibold'>Education: </span>
         {education.map((edu,index)=>(
-          <li key={index} className='px-2 flex ml-4 font-bold text-rose-800 rounded-lg'>{index+1}. {edu.College}- {edu.Degree} ({edu.CGPA})
+          <li key={index} className='px-2 flex ml-4 font-bold text-rose-600 rounded-lg'>{index+1}. {edu.College}- {edu.Degree} ({edu.CGPA})
           </li>
         ))}
           </ul>
           <ul className='flex flex-col mt-6'><span className='font-semibold'>Experience: </span>
         {experience.map((exp,index)=>(
-          <li key={index} className='px-2 flex ml-4 font-bold text-rose-800 rounded-lg'>{index+1}. {exp.title}: {exp.Duration}<br/>{exp.Description}
+          <li key={index} className='px-2 flex ml-4 font-bold text-rose-600 rounded-lg'>{index+1}. {exp.title}: {exp.Duration}<br/>{exp.Description}
           </li>
         ))}
           </ul>
